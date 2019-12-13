1313478e3fdc5f084ad28a6500050733da5f81be
Isabelle Elisie da Silva Santos <isabelleelisiedasilvasantos@MacBook-Air-de-Isabelle.local>
2019-12-13 14:44:33 +0100

    Score calcul

--- src/main/java/formation/xp/models/Score.java
+++ src/main/java/formation/xp/models/Score.java
@@ -2,76 +2,116 @@ package formation.xp.models;
 
 public class Score {
 	
-	public int score_total;
-	public int tot_q;
-	public int status_affiche;
-	public int score_affiche;
-	public int set;
-	public int lancer;
+	public Joueur joueur;
 	
-	public Score()
+	public Score(Joueur J)
 	{
-		score_total=0;
-		score_affiche=0;
-		set=1;
-		lancer=1;
-		tot_q=0;
-		status_affiche= 0;
+		joueur=J;
 	}
 	
-	public void mis_a_jour_set()
+	public int calculScore()
 	{
-		set++;
-		lancer=1;
-	}
-	
-	public void mis_a_jour_lancer()
-	{
-		lancer=2;
-	}
-	
-	public void quilles_tombe(int quilles)
-	{
-		if(lancer==1)
+		int i;
+		int score=0;
+		boolean strike=true;
+		
+		
+		for(i=joueur.turns.size()-1; i>=0; i--)
 		{
-			tot_q=quilles;
-			if(tot_q==10)
-			{
-				score_total+=10;
-				score_affiche=score_total;
-				status_affiche=2;
-			}
-			else
-			{
-				mis_a_jour_lancer();
-			}
-		}
-		else
-		{
-			tot_q+=quilles;
-			if(tot_q==10)
-			{
-				score_total+=10;
-				score_affiche=score_total;
-				status_affiche=1;
-			}
-			else
-			{
-				score_total+=tot_q;
-				score_affiche=score_total;
-				status_affiche=0;
-				mis_a_jour_set();
-			}
 			
+			if(joueur.turns.size()<10)
+			{
+				
+				if((joueur.turns.get(i).lancer1)!=10)
+				{
+					strike=false;
+				}
+				
+				if(!strike)
+				{
+					if(joueur.turns.get(i).lancer1!=10)
+					{
+						if((joueur.turns.get(i).lancer1+joueur.turns.get(i).lancer2)!=10)
+						{
+							score+=joueur.turns.get(i).lancer1;
+							score+=joueur.turns.get(i).lancer2;
+						}
+						else
+						{
+							score+=10;
+							if(i!=9)
+							{
+								score+=joueur.turns.get(i+1).lancer1;
+							}else
+							{
+								score+=joueur.turns.get(i).lancer3;
+							}
+						}
+					}else
+					{
+						score+=10;
+						
+						if(joueur.turns.get(i+1).lancer1==10)
+						{
+							score+=10;	
+							score+=joueur.turns.get(i+2).lancer1;
+						}
+						else
+						{
+							score+=joueur.turns.get(i+1).lancer1+joueur.turns.get(i+1).lancer2;
+						}
+					}
+				}
+			}else
+			{
+				if(joueur.turns.get(i).lancer1!=10)
+				{
+
+					if((joueur.turns.get(i).lancer1+joueur.turns.get(i).lancer2)!=10)
+					{
+						score+=joueur.turns.get(i).lancer1;
+						score+=joueur.turns.get(i).lancer2;
+					}
+					else
+					{
+						score+=10;
+						if(i!=9)
+						{
+							score+=joueur.turns.get(i+1).lancer1;
+						}else
+						{
+							score+=joueur.turns.get(i).lancer3;
+						}
+					}
+				}else
+				{
+					score+=10;
+					
+					if(i==9)
+					{
+						score+=joueur.turns.get(i).lancer2+joueur.turns.get(i).lancer3;	
+					}else if(i==8)
+					{
+						score+=joueur.turns.get(i+1).lancer1+joueur.turns.get(i+1).lancer2;	
+					}else
+						{
+							if(joueur.turns.get(i+1).lancer1==10)
+							{
+								score+=10;		
+								score+=joueur.turns.get(i+2).lancer1;
+							}
+							else
+							{
+								score+=joueur.turns.get(i+1).lancer1+joueur.turns.get(i+1).lancer2;
+							}
+						}
+				}
+			}	
 		}
+		
+		return score;
 	}
 	
-	public void afficher()
-	{
-		if(status_affiche==0)
-		{
-			System.out.printf("Score: %d \n", score_affiche);
-		}
-	}
+	
 	
 }
--- src/test/java/formation/xp/models/TourTest.java
+++ src/test/java/formation/xp/models/TourTest.java
@@ -1,6 +1,5 @@
 package formation.xp.models;
 
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
