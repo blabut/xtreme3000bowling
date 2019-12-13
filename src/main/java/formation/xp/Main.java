31babb3a63e5ffa2d841819f25e75d967c964a56
paul <pazoulai@gmail.com>
2019-12-13 09:48:41 +0100

    modifies main

--- src/main/java/formation/xp/Main.java
+++ src/main/java/formation/xp/Main.java
@@ -2,6 +2,6 @@ package formation.xp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("YOLOOOO");
+        System.out.println("Please launch ta boule now");
     }
 }
