58ab44a3a5c0dfd033dfffbab30338873bc69a0e
Antoine Pinaud <antoinepm.pinaud@laposte.net>
2019-12-13 10:25:37 +0100

    adds Tour class

--- src/main/java/formation/xp/models/Game.java
+++ src/main/java/formation/xp/models/Game.java
@@ -1,6 +1,11 @@
 package formation.xp.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
     Joueur joueur;
+    List<Tour> tours = new ArrayList<Tour>();
+    int score;
     
 }
---
+++ src/main/java/formation/xp/models/Tour.java
@@ -0,0 +1,7 @@
+package formation.xp.models;
+
+public class Tour {
+
+    int nbQuillesTombees1;
+    int nbQuillesTombees2;
+}
